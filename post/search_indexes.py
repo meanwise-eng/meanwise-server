2d0c28e49c265a33d83257753c02aef98f54afb0
raj <raj.emailme@gmail.com>
2017-01-06 10:44:03 +0530

    Added post file upload example

--- post/search_indexes.py
+++ post/search_indexes.py
@@ -5,7 +5,7 @@ from post.models import Post
 class PostIndex(indexes.SearchIndex, indexes.Indexable):
 
     text = indexes.CharField(document=True, use_template=False)
-    post_text = indexes.CharField(model_attr="text")
+    post_text = indexes.CharField(model_attr="text", null=True)
     id = indexes.CharField(model_attr="id")
     interest_name = indexes.CharField()
     #interest = 
