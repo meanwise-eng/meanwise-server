47d6dc7816aa289f38914607c0978644bb2a1639
raj <raj.emailme@gmail.com>
2017-02-06 18:06:30 +0530

    Added code to generate video thumbnail

--- post/models.py
+++ post/models.py
@@ -1,12 +1,15 @@
 from __future__ import unicode_literals
+import os
 
 from django.db import models
 from django.conf import settings
 
 from taggit.managers import TaggableManager
 from common.utils import slugify
+from moviepy.editor import *
 
 from django.contrib.auth.models import User
+from django.core.files import File
 
 from userprofile.models import Interest
 
@@ -27,8 +30,18 @@ class Post(models.Model):
 
     def __str__(self):
         return "Post id: " + str(self.id)  + " poster: " + str(self.poster)
-    
 
+    def save(self, *args, **kwargs):
+        if self.video:
+            if not self.video_thumbnail:
+                super(Post, self).save(*args, **kwargs)
+                clip = VideoFileClip(self.video.path)
+                thumbnail_path = os.path.join(os.path.dirname(self.video.path), os.path.splitext(os.path.basename(self.video.name))[0]) + ".jpg"
+                clip.save_frame(thumbnail_path , t=1.00)
+                _file = File(open(thumbnail_path, "rb"))
+                self.video_thumbnail.save((os.path.splitext(os.path.basename(self.video.name))[0] + ".jpg"), _file, save=True)
+        super(Post, self).save(*args, **kwargs)
+  
 class Comment(models.Model):
     post = models.ForeignKey(Post, db_index=True)
     commented_by = models.ForeignKey(User)
