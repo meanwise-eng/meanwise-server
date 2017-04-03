8bea483ed34af7b24bef73cf000143cecabe7024
raj <raj.emailme@gmail.com>
2017-04-03 14:26:10 +0530

    Added facebook_token to fetch token api

--- custom_auth/views.py
+++ custom_auth/views.py
@@ -79,21 +79,28 @@ class FetchToken(APIView):
         try:
             email = request.data.get('email', None)
             password = request.data.get('password', None)
+            facebook_token = request.data.get('facebook_token', None)
             try:
                 user = User.objects.get(email=email)
             except User.DoesNotExist:
                 return Response({"status":"failed", "error":"User with email does not exist", "results":""}, status.HTTP_400_BAD_REQUEST)
 
-            #check password
-            isvalid = user.check_password(password)
-            print ("isvalid", isvalid)
-            if not isvalid:
-                return Response({"status":"failed", "error":"User password did not match", "results":""}, status.HTTP_400_BAD_REQUEST)
-
+            if password:
+                #check password
+                isvalid = user.check_password(password)
+                if not isvalid:
+                    return Response({"status":"failed", "error":"User password did not match", "results":""}, status.HTTP_400_BAD_REQUEST)
+            elif facebook_token:
+                try:
+                    up = UserProfile.objects.get(user=user)
+                except UserProfile.DoesNotExist:
+                    return Response({"status":"failed", "error":"UserProfile for user not found", "results":""}, status.HTTP_400_BAD_REQUEST)
+                if facebook_token !=  up.facebook_token:
+                    return Response({"status":"failed", "error":"User fb token did not match", "results":""}, status.HTTP_400_BAD_REQUEST)
             try:
                 token = Token.objects.get(user=user)
             except Token.DoesNotExist:
-                return Response({"status":"failed", "error":"Toek for User with email does not exist", "results":""}, status.HTTP_400_BAD_REQUEST)
+                return Response({"status":"failed", "error":"Token for User with email does not exist", "results":""}, status.HTTP_400_BAD_REQUEST)
             data = {"token":token.key, "user_id":user.id}
 
             return Response({"status":"success", "error":"", "result":data} ,  status.HTTP_200_OK)
