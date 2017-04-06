c6101aefade30f9851f56a92883813874d2e1008
raj <raj.emailme@gmail.com>
2017-04-06 10:41:07 +0530

    Enabled pagination

--- post/views.py
+++ post/views.py
@@ -35,13 +35,12 @@ class UserPostList(APIView):
 
     def get(self, request, user_id):
         posts = Post.objects.filter(is_deleted=False).filter(poster__id=user_id).order_by('-created_on')
-        #page = request.GET.get('page')
-        #page_size = request.GET.get('page_size')
-        #posts, has_next_page, num_pages  = get_objects_paginated(posts, page, page_size)
+        page = request.GET.get('page')
+        page_size = request.GET.get('page_size')
+        posts, has_next_page, num_pages  = get_objects_paginated(posts, page, page_size)
         serializer = PostSerializer(posts, many=True)
-        #return Response({"status":"success", "error":"", "results":{"data":serializer.data, "num_pages":num_pages}}, status=status.HTTP_200_OK)
-        return Response({"status":"success", "error":"", "results":serializer.data}, status=status.HTTP_200_OK)
-
+        return Response({"status":"success", "error":"", "results":{"data":serializer.data, "num_pages":num_pages}}, status=status.HTTP_200_OK)
+        
     def post(self, request, user_id):
         data = request.data
         request.data['poster'] = user_id
@@ -142,12 +141,11 @@ class UserHomeFeed(APIView):
                 return Response({"status":"failed", "error":"Error fetching userprofile/interests for user.", "results":""}, status=status.HTTP_400_BAD_REQUEST)
             interests_ids = userprofile.interests.all().values_list('id', flat=True)
             home_feed_posts = Post.objects.filter(is_deleted=False).filter(Q(poster__id__in=friends_ids) | Q(interest__id__in=interests_ids) | Q(poster__id=user_id)).order_by('-created_on')
-            #page = request.GET.get('page')
-            #page_size = request.GET.get('page_size')
-            #home_feed_posts, has_next_page, num_pages  = get_objects_paginated(home_feed_posts, page, page_size)
+            page = request.GET.get('page')
+            page_size = request.GET.get('page_size')
+            home_feed_posts, has_next_page, num_pages  = get_objects_paginated(home_feed_posts, page, page_size)
             serializer = PostSerializer(home_feed_posts, many=True)
-            #return Response({"status":"success", "error":"", "results":{"data":serializer.data, "num_pages":num_pages}}, status=status.HTTP_200_OK)
-            return Response({"status":"success", "error":"", "results":serializer.data}, status=status.HTTP_200_OK)
+            return Response({"status":"success", "error":"", "results":{"data":serializer.data, "num_pages":num_pages}}, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"status":"failed", "error":str(e), "results":""}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -224,13 +222,11 @@ class PostCommentList(APIView):
 
     def get(self, request, post_id):
         comments = Comment.objects.filter(is_deleted=False).filter(post__id=post_id).order_by('-created_on')
-        #page = request.GET.get('page')
-        #page_size = request.GET.get('page_size')
-        #comments, has_next_page, num_pages  = get_objects_paginated(comments, page, page_size)
+        page = request.GET.get('page')
+        page_size = request.GET.get('page_size')
+        comments, has_next_page, num_pages  = get_objects_paginated(comments, page, page_size)
         serializer = CommentSerializer(comments, many=True)
-        #return Response({"status":"success", "error":"", "results":{"data":serializer.data, "num_pages":num_pages}}, status=status.HTTP_200_OK)
-        return Response({"status":"success", "error":"", "results":serializer.data}, status=status.HTTP_200_OK)
-    
+        return Response({"status":"success", "error":"", "results":{"data":serializer.data, "num_pages":num_pages}}, status=status.HTTP_200_OK)
 
     def post(self, request, post_id):
         data = request.data
