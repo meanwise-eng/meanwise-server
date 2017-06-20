01e437d7c07f85fc91c43f30526a6aa6ec8965f9
Amjad Mohamed <amjad@inglab.com.my>
2017-06-20 15:38:51 +0500

    Exclude the media folder when building docker image

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 export COMPOSE_PROJECT_NAME=meanwisebuild
 
-rsync -avz --exclude=docker --exclude=.git --exclude=docker-compose*.yml --exclude=build.sh ./ ./docker/build/app/code
+rsync -avz --exclude=media --exclude=docker --exclude=.git --exclude=docker-compose*.yml --exclude=build.sh ./ ./docker/build/app/code
 
 docker-compose -f docker-compose.build.yml build
 
