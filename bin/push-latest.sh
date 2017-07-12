e941b80e52859d707adc1012bd10881418514788
Amjad Mohamed <amjad@inglab.com.my>
2017-07-12 22:42:37 +0800

    Changed the push script to accept version and tag

--- bin/push-latest.sh
+++ bin/push-latest.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-docker tag meanwise/api:latest 033556216955.dkr.ecr.us-east-1.amazonaws.com/meanwise/api:latest || exit
-docker push 033556216955.dkr.ecr.us-east-1.amazonaws.com/meanwise/api:latest
+echo "Specify the version:"
+read VERSION
+
+echo "Specify label (eg. latest) tag:"
+read TAG
+
+if [ $VERSION != "" ]; then
+	echo "Pushing version $VERSION"
+	docker tag meanwise/api:latest 033556216955.dkr.ecr.us-east-1.amazonaws.com/meanwise/api:$VERSION || exit
+	docker push 033556216955.dkr.ecr.us-east-1.amazonaws.com/meanwise/api:$VERSION
+fi
+
+if [ $TAG != "" ]; then
+	echo "Pushing version $TAG"
+	docker tag meanwise/api:latest 033556216955.dkr.ecr.us-east-1.amazonaws.com/meanwise/api:$TAG || exit
+	docker push 033556216955.dkr.ecr.us-east-1.amazonaws.com/meanwise/api:$TAG
+fi
