1facf68187db6da952e8f6bcfb2bb653c4ce1fbf
Amjad Mohamed <amjad@inglab.com.my>
2017-07-17 18:27:15 +0800

    Removed relevance from post serializer as relevance is only available for home feed

--- post/serializers.py
+++ post/serializers.py
@@ -34,7 +34,6 @@ class PostSerializer(TaggitSerializer, serializers.ModelSerializer):
         lookup_url_kwarg='story_id',
         view_name='post-story'
     )
-    relevance=serializers.IntegerField()
 
     queryset=Post.objects.filter(is_deleted=False)
     
@@ -43,7 +42,7 @@ class PostSerializer(TaggitSerializer, serializers.ModelSerializer):
         fields = ('id', 'text', 'user_id', 'num_likes', 'num_comments', 'interest_id', 'user_firstname', 'user_lastname',
                       'user_profile_photo', 'user_cover_photo', 'user_profile_photo_small', 'user_profession', 'user_profession_text',
                       'image_url', 'video_url', 'video_thumb_url', 'resolution', 'created_on', 'tags', 'topics',
-                      'story', 'story_index', 'is_liked', 'likes_url', 'relevance')
+                      'story', 'story_index', 'is_liked', 'likes_url')
 
     def get_user_id(self, obj):
         user_id = obj.poster.id
