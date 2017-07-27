c08025c6572c538d15fe0c161292f2593964b1c8
PunitGr <groove679@gmail.com>
2017-07-27 13:24:54 +0530

    Add daterange to PersonalAnalytics View

--- analytics/urls.py
+++ analytics/urls.py
@@ -3,5 +3,5 @@ from .views import PostAnalyticsView, PersonalAnalyticsView
 
 urlpatterns = [
     url(r'^$', PostAnalyticsView.as_view()),
-    url(r'^user/(?P<user_id>[0-9]+)/$', PersonalAnalyticsView.as_view()),
+    url(r'^user/$', PersonalAnalyticsView.as_view()),
 ]
--- analytics/views.py
+++ analytics/views.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime, timedelta
 
 from django.core.exceptions import PermissionDenied
 from django.db.models import Count
@@ -92,55 +93,71 @@ class PersonalAnalyticsView(APIView):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
-    def get(self, request, user_id):
+    def get(self, request):
 
-        if int(user_id) == request.user.id:
-            try:
-                UserProfile.objects.get(user__id=user_id)
+        kwargs = {}
+        user_id = request.GET.get("user")
+        date_from = request.GET.get("date_from")
+        date_to = request.GET.get("date_to")
 
-            except UserProfile.DoesNotExist:
+        if user_id is not None:
+            kwargs['poster'] = int(user_id)
+
+            if date_from is not None:
+                kwargs['datetime__gte'] = date_from
+
+            if date_to is not None:
+                date_to = datetime.strptime(date_to, "%Y-%m-%d")
+                date_to = date_to + timedelta(days=1)
+                kwargs['datetime__lte'] = date_to
+
+            if int(user_id) == request.user.id:
+                try:
+                    UserProfile.objects.get(user__id=int(user_id))
+
+                except UserProfile.DoesNotExist:
+                    return Response(
+                        {
+                            "status": "failed",
+                            "error": {
+                                "message": "Error occured on server.",
+                                "code": 500,
+                                "subCode": 1,
+                                "errorTitle": "Error occured on server",
+                                "errorMessage": "An unidentified error occured on server. We will be looking into this issue. Please try again later."
+                            },
+                            "results": ""
+                        },
+                        status=status.HTTP_501_NOT_IMPLEMENTED
+                    )
+
+                posts = SeenPost.objects.filter(**kwargs)
+                serializer = SeenPostSerializer(posts, many=True)
+                data = json.loads(json.dumps(serializer.data))
+
+                for i in range(len(data)):
+                    post_id = data[i]["post_id"]
+
+                    like_queryset = Post.objects.filter(id=post_id).values("liked_by", likes=Count("liked_by"))
+                    likes = (list(like_queryset)[0]["likes"])
+
+                    comment_queryset = Comment.objects.filter(post=post_id)
+                    comments = len(list(comment_queryset))
+
+                    data[i]["no_of_likes"] = likes
+                    data[i]["no_of_comments"] = comments
+
                 return Response(
                     {
-                        "status": "failed",
-                        "error": {
-                            "message": "Error occured on server.",
-                            "code": 500,
-                            "subCode": 1,
-                            "errorTitle": "Error occured on server",
-                            "errorMessage": "An unidentified error occured on server. We will be looking into this issue. Please try again later."
-                        },
-                        "results": ""
+                        "status": "success",
+                        "error": "",
+                        "results": {"posts": data}
                     },
-                    status=status.HTTP_501_NOT_IMPLEMENTED
+                    status=status.HTTP_200_OK
                 )
 
-            posts = SeenPost.objects.filter(poster=int(user_id))
-            serializer = SeenPostSerializer(posts, many=True)
-            data = json.loads(json.dumps(serializer.data))
-
-            for i in range(len(data)):
-                post_id = data[i]["post_id"]
-
-                like_queryset = Post.objects.filter(id=post_id).values("liked_by", likes=Count("liked_by"))
-                likes = (list(like_queryset)[0]["likes"])
-
-                comment_queryset = Comment.objects.filter(post=post_id)
-                comments = len(list(comment_queryset))
-
-                data[i]["no_of_likes"] = likes
-                data[i]["no_of_comments"] = comments
-
-            return Response(
-                {
-                    "status": "success",
-                    "error": "",
-                    "results": {"posts": data}
-                },
-                status=status.HTTP_200_OK
-            )
-
-        else:
-            raise PermissionDenied("You cannot view analytics of other users")
+            else:
+                raise PermissionDenied("You cannot view analytics of other users")
 
         return Response(
             {
