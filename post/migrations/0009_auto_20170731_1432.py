b94615a74473010065098d570cb0f853bf76835d
Amjad Mohamed <amjad@meanwise.com>
2017-08-05 00:09:41 +0800

    Fixed migrations conflict

--- post/migrations/0009_auto_20170731_1432.py
+++ post/migrations/0009_auto_20170731_1432.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('post', '0005_auto_20170622_0922'),
+        ('post', '0008_auto_20170802_1433'),
     ]
 
     operations = [
