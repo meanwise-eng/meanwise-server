b5b40b3858c6b11ad9b62a7624339f505dd859df
Andho Mohamed <amjad@meanwise.com>
2017-10-29 19:41:35 +0500

    Added logging to push notification function

--- common/push_message.py
+++ common/push_message.py
@@ -1,8 +1,12 @@
+import logging
+
 from scarface.models import Device, PushMessage
 
 from mnotifications.models import ASNSDevice
 
 from django.conf import settings
+
+logger = logging.getLogger('meanwise_backend.%s' % __name__)
 
 
 def find_user_devices(user_id):
@@ -32,9 +36,11 @@ def send_message_device(device, message_payload):
         message = PushMessage(badge_count=1, context='url_alert', context_id='none',
                               has_new_content=True, message=message, sound="default")
     except Exception as e:
+        logger.error(e)
         return 0
     try:
         device.send(message, extra_pload={'p': message_payload[
                     'p'], 'u': message_payload['u'], 't': message_payload['t']})
     except Exception as e:
+        logger.error(e)
         return 0
