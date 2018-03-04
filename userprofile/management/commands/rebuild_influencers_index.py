7d7314b414c45532fe9d7bfabdadccd14ebacbef
Andho Mohamed <amjad@meanwise.com>
2018-03-04 14:03:13 +0500

    Manually convert topics to upper case before adding to influencer index

--- userprofile/management/commands/rebuild_influencers_index.py
+++ userprofile/management/commands/rebuild_influencers_index.py
@@ -48,8 +48,8 @@ class Command(BaseCommand):
                 influencer.popularity_weeky = (weekly_comments_count * 5)
                 influencer.popularity_overall = (old_comments_count * 5) + (likes * 3)
 
-            influencer.topics_overall = list(posts.values_list('topic', flat=True))
-            influencer.topics_weekly = list(posts.values_list('topic', flat=True))
+            influencer.topics_overall = [t.upper() for t in posts.values_list('topic', flat=True)]
+            influencer.topics_weekly = [t.upper() for t in posts.values_list('topic', flat=True)]
 
             try:
                 boost = userprofile.profile_boosts.latest('boost_datetime')
